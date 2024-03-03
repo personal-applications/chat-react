7cc4869523a6282482ae2ad26c1b8959cf5da00d
Phạm Thành Trung <adamward459@gmail.com>
2024-03-03 23:27:37 +0700

    🎨 style: update styling and placeholders in sign up form
    - Updated the title to "ChatHub"
    - Updated the styling of the form container
    - Updated the styling and placeholders of the input fields
    - Added a button for signing up

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,36 @@ function App() {
     <>
       <div className="h-screen w-screen bg-neutral pt-40">
         <div className="w-96 mx-auto">
-          <h1 className="text-xl font-semibold text-center">Sign up</h1>
-          <div className="bg-base-100 mt-3 p-4 rounded-sm">
-            <div className="flex gap-0.5">
-
-            <input
-              type="text"
-              placeholder="First name"
-              className="input input-bordered w-full max-w-xs"
-            />
-            <input
-              type="text"
-              placeholder="Last name"
-              className="input input-bordered w-full max-w-xs"
-            />
+          <h1 className="text-xl font-semibold text-center">ChatHub</h1>
+          <div className="bg-base-100 mt-3 p-5 rounded-md flex flex-col gap-y-3">
+            <div className="flex gap-x-4">
+              <input
+                type="text"
+                placeholder="First name"
+                className="input input-bordered w-full"
+              />
+              <input
+                type="text"
+                placeholder="Last name"
+                className="input input-bordered w-full"
+              />
             </div>
             <input
               type="text"
               placeholder="Email"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full"
             />
             <input
               type="text"
               placeholder="Password"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full"
             />
             <input
               type="text"
               placeholder="Confirm password"
-              className="input input-bordered w-full max-w-xs"
+              className="input input-bordered w-full"
             />
+            <button className="btn btn-primary">Sign up</button>
           </div>
         </div>
       </div>
