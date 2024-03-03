91a4cc3cb4e394ca48aef9b998e353d8949ba5a2
Pham Thanh Trung <adamward459@gmail.com>
2024-03-03 23:28:11 +0700

    🚀 feat: update dependencies (#2)
    * 🚀 feat: update dependencies
    
    - Added autoprefixer version 10.4.18
    - Added daisyui version 4.7.2
    - Added postcss version 8.4.35
    - Added tailwindcss version 3.4.1
    - Updated eslint version to 8.56.0
    - Updated typescript version to 5.2.2
    - Updated vite version to 5.1.4
    
    * 🎨 style: update login form UI and remove unused code
    
    The changes in this commit update the UI of the login form in the client application. The changes include adding styling classes to the form inputs and wrapping the form in a container with a background color. Additionally, unused code related to the logo animation and styling has been removed. These changes improve the visual appearance of the login form and remove unnecessary code clutter.
    
    * 🔧 fix: update ESLint configuration to use double quotes consistently
    
    * 💄 style: update sign up form layout
    
    - Added a container div with flex gap for better alignment of form inputs.
    - Wrapped the "User name" label and inputs in a div to keep them together.
    - Added a closing div tag to close the container div.
    
    * 🎨 style: update styling and placeholders in sign up form
    
    - Updated the title to "ChatHub"
    - Updated the styling of the form container
    - Updated the styling and placeholders of the input fields
    - Added a button for signing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-screen bg-neutral pt-40">
+        <div className="w-96 mx-auto">
+          <h1 className="text-xl font-semibold text-center">ChatHub</h1>
+          <div className="bg-base-100 mt-3 p-5 rounded-md flex flex-col gap-y-3">
+            <div className="flex gap-x-4">
+              <input
+                type="text"
+                placeholder="First name"
+                className="input input-bordered w-full"
+              />
+              <input
+                type="text"
+                placeholder="Last name"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              className="input input-bordered w-full"
+            />
+            <input
+              type="text"
+              placeholder="Confirm password"
+              className="input input-bordered w-full"
+            />
+            <button className="btn btn-primary">Sign up</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
