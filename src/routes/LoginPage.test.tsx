89c5540a65338281be8da85575d6210f76f4896a
Phạm Thành Trung <adamward459@gmail.com>
2024-03-17 19:24:06 +0700

    🔧 Add unit test for ForgotPasswordPage component using Vitest framework.

--- src/routes/LoginPage.test.tsx
+++ src/routes/LoginPage.test.tsx
@@ -11,6 +11,7 @@ vi.mock("swr/mutation", () => {
     default: vi.fn().mockReturnValue({}),
   };
 });
+
 vi.mock("react-router-dom", async (importOriginal) => {
   const mod = await importOriginal<typeof import("react-router-dom")>();
   return {
@@ -19,47 +20,42 @@ vi.mock("react-router-dom", async (importOriginal) => {
   };
 });
 
-suite("LogInPage", () => {
-  test("should render the login form", () => {
-    const tree = render(
-      <BrowserRouter>
-        <LogInPage />
-      </BrowserRouter>,
-    );
+test("should render the login form", () => {
+  const tree = render(
+    <BrowserRouter>
+      <LogInPage />
+    </BrowserRouter>,
+  );
 
-    expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
+});
+
+test("should navigate to /home after successful login", async () => {
+  const navigateMock = vi.fn();
+  (useNavigate as Mock).mockReturnValue(navigateMock);
+
+  const triggerMock = vi.fn().mockResolvedValue({ jwt: "dummyToken" });
+  (useSWRMutation as Mock).mockReturnValue({
+    trigger: triggerMock,
   });
 
-  test("should navigate to /home after successful login", async () => {
-    const navigateMock = vi.fn();
-    (useNavigate as Mock).mockReturnValue(navigateMock);
+  render(
+    <BrowserRouter>
+      <LogInPage />
+    </BrowserRouter>,
+  );
 
-    const triggerMock = vi.fn().mockResolvedValue({ jwt: "dummyToken" });
-    (useSWRMutation as Mock).mockReturnValue({
-      trigger: triggerMock,
-    });
+  await userEvent.type(
+    screen.getByPlaceholderText("Email"),
+    "test@example.com",
+  );
+  await userEvent.type(screen.getByPlaceholderText("Password"), "Password123*");
+  await userEvent.click(screen.getByText("Log in"));
 
-    render(
-      <BrowserRouter>
-        <LogInPage />
-      </BrowserRouter>,
-    );
-
-    await userEvent.type(
-      screen.getByPlaceholderText("Email"),
-      "test@example.com",
-    );
-    await userEvent.type(
-      screen.getByPlaceholderText("Password"),
-      "Password123*",
-    );
-    await userEvent.click(screen.getByText("Log in"));
-
-    expect(getFromLocalStorage("jwt")).toBe("dummyToken");
-    expect(triggerMock).toHaveBeenCalledWith({
-      email: "test@example.com",
-      password: "Password123*",
-    });
-    expect(navigateMock).toHaveBeenCalledWith("/home");
+  expect(getFromLocalStorage("jwt")).toBe("dummyToken");
+  expect(triggerMock).toHaveBeenCalledWith({
+    email: "test@example.com",
+    password: "Password123*",
   });
+  expect(navigateMock).toHaveBeenCalledWith("/home");
 });
