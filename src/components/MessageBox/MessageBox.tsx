c4ebd4855fd1f0dd67e3c1fa8cafe2286e65a03d
Phạm Thành Trung <adamward459@gmail.com>
2024-04-01 22:20:58 +0700

    ✨ Add loading spinner when submitting message.

--- src/components/MessageBox/MessageBox.tsx
+++ src/components/MessageBox/MessageBox.tsx
@@ -1,9 +1,25 @@
+import { postData } from "../../services/fetcher";
 import SendMessageForm, { SendMessageFields } from "./SendMessageForm";
+import useSWRMutation from "swr/mutation";
+
+export type CreateMessageBody = SendMessageFields & {
+  receiverId: number;
+};
 
 export default function MessageBox() {
-  const onSubmit = (data: SendMessageFields) => {
-    console.log(data);
-    return Promise.resolve();
+  const { trigger } = useSWRMutation<
+    { id: number },
+    Error,
+    "/api/messages",
+    CreateMessageBody
+  >("/api/messages", postData);
+
+  const onSubmit = async (data: SendMessageFields) => {
+    const body: CreateMessageBody = {
+      content: data.content,
+      receiverId: 1,
+    };
+    await trigger(body);
   };
   return (
     <div>
